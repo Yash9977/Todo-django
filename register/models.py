b402935236c317bb22de04160148e9d4d5e27eec
Yash9977 <0808cs211206.ies@ipsacademy.org>
2024-06-20 20:46:35 +0530

    todo

--- register/models.py
+++ register/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class register(models.Model):
+    username=models.ForeignKey('auth.user',on_delete=models.DO_NOTHING,null=True)
     name=models.CharField(max_length=100)
     age=models.IntegerField()
     phone=models.IntegerField()
